07a5f62170c04bb2bc901c0ea6950db49945c45c
Trina Hoang Ngo <trinahngo311@gmail.com>
2024-12-04 01:53:21 -0800

    computer choice function

--- play.js
+++ play.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let rock = 0;
+    let paper = 1;
+    let scissors = 2;
+
+    return Math.floor(Math.random() * 3);
+}
+
+console.log();
